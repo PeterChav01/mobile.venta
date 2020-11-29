78cc767b76df84bb4d57e218edc2d2b081b81ade
Emmanuel Gonzalez <jgonzalez13@ucol.mx>
2020-11-29 11:32:15 -0600

    boton to dashbord

--- app/src/main/java/com/example/teamexam/MainActivity.kt
+++ app/src/main/java/com/example/teamexam/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.teamexam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -8,5 +9,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        boton1.setOnClickListener {
+            val intento1 = Intent(this, Dashboard::class.java)
+            startActivity(intento1)
+
+        }
     }
 }
