1129e50c6197f532139f09a1e651512b3dc0780e
Emmanuel Gonzalez <jgonzalez13@ucol.mx>
2020-11-29 20:39:52 -0600

    login in users

--- app/src/main/java/com/example/teamexam/MainActivity.kt
+++ app/src/main/java/com/example/teamexam/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.teamexam
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,15 +12,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         boton1.setOnClickListener {
+            val admin = AdminSQLiteOpenHelper(this, "administracion", null, 1)
+            val bd = admin.writableDatabase
             val intento1 = Intent(this, Dashboard::class.java)
-            startActivity(intento1)
 
+            val fila = bd.rawQuery("select id from users where user='${editText.text.toString()}' and password='${editText2.text.toString()}'", null)
+            if (fila.moveToFirst()) {
+                startActivity(intento1)
+            } else
+                Toast.makeText(this, "El usuario no existe",  Toast.LENGTH_SHORT).show()
+            bd.close()
         }
 
         boton2.setOnClickListener {
             val intento1 = Intent(this, Registrarse::class.java)
             startActivity(intento1)
-
         }
+
+
     }
 }
--- app/src/main/java/com/example/teamexam/Registrarse.kt
+++ app/src/main/java/com/example/teamexam/Registrarse.kt
@@ -2,10 +2,33 @@ package com.example.teamexam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.ContentValues
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_registrarse.*
 
 class Registrarse : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrarse)
+
+        boton2.setOnClickListener {
+            val admin = AdminSQLiteOpenHelper(this,"administracion", null, 1)
+            val bd = admin.writableDatabase
+            val registro = ContentValues()
+
+            registro.put("user", editText.getText().toString())
+            registro.put("password", editText2.getText().toString())
+            registro.put("email", editText3.getText().toString())
+            registro.put("store", editText4.getText().toString())
+            bd.insert("users", null, registro)
+            bd.close()
+
+            editText.setText("")
+            editText2.setText("")
+            editText3.setText("")
+            editText4.setText("")
+
+            Toast.makeText(this, "Se registro el usuario y la tienda", Toast.LENGTH_SHORT).show()
+        }
     }
 }
