453ff43c1b75449ffaf659318f4e017d2d0c1fd5
PeterChav01 <sahdow_nfsc@yahoo.com.mx>
2020-11-30 23:44:22 -0600

    added product for spinner

--- app/src/main/java/com/example/teamexam/Ventas.kt
+++ app/src/main/java/com/example/teamexam/Ventas.kt
@@ -1,11 +1,13 @@
 package com.example.teamexam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_clientes.*
+import kotlinx.android.synthetic.main.activity_dashboard.*
 import kotlinx.android.synthetic.main.activity_ventas.*;
 
 
@@ -16,10 +18,12 @@ class Ventas : AppCompatActivity() {
         setContentView(R.layout.activity_ventas)
 
         var listaClientes = ArrayList<String>();
+        var listaProductos = ArrayList<String>();
         var n = 0;
         val admin = AdminSQLiteOpenHelper(this, "administracionn", null, 1)
         val bd = admin.writableDatabase
         val fila = bd.rawQuery("select nombre from cliente", null)
+        val filaproductos = bd.rawQuery("select nombre from productos", null)
 
         if (fila.moveToFirst()) {
             while (n < fila.count) {
@@ -33,10 +37,27 @@ class Ventas : AppCompatActivity() {
         } else {
             Toast.makeText(this, "No existe un Cliente con dicho nombre", Toast.LENGTH_SHORT).show()
         }
-        
+
         n=0
+
+        if (filaproductos.moveToFirst()) {
+            while (n < filaproductos.count) {
+                n++
+                listaProductos.add(filaproductos.getString(0))
+                fila.moveToNext()
+            }
+            val adaptador2 = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, listaProductos)
+            spinner2.adapter=adaptador2
+
+        } else {
+            Toast.makeText(this, "No existe un Producto con dicho nombre", Toast.LENGTH_SHORT).show()
+        }
 
         bd.close()
 
+        buton1.setOnClickListener {
+            et1.setText("")
+            Toast.makeText(this, "Venta Generada", Toast.LENGTH_SHORT).show()
+        }
     }
 }
