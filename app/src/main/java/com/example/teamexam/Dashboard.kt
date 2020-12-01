d6b9cd74183fe8274ebfe1890bcbd9afe1f84bb6
Emmanuel Gonzalez <jgonzalez13@ucol.mx>
2020-11-30 20:45:27 -0600

    oscar changes

--- app/src/main/java/com/example/teamexam/Dashboard.kt
+++ app/src/main/java/com/example/teamexam/Dashboard.kt
@@ -4,11 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_dashboard.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class Dashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        imgbtn2.setOnClickListener {
+            val intento1 = Intent(this, Clientes::class.java)
+            startActivity(intento1)
+        }
 
         imgbtn3.setOnClickListener {
             val intento1 = Intent(this, Ventas::class.java)
@@ -17,6 +23,12 @@ class Dashboard : AppCompatActivity() {
 
         imgbtn4.setOnClickListener {
             val intento1 = Intent(this, Reportes::class.java)
+            startActivity(intento1)
+        }
+
+
+        boton3.setOnClickListener {
+            val intento1 = Intent(this, MainActivity::class.java)
             startActivity(intento1)
         }
 
